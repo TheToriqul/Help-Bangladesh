e47bd2379fae064bac103fd5372afdacf96dcc44
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-23 01:23:35 +0800

    Feat: Faq page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // This is my boilerplate default configuration for Tailwind CSS and daisyUI
-  content: ["./index.html", "./JavaScript/**/*.{html,js}"],
+  content: ["./index.html", "./JavaScript/**/*.{html,js}", "./faqs.html"],
   theme: {
     extend: {
       fontFamily: {
